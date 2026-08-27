3f5860703173d3e63bcdebb5a2e6f3b8765d7111
Sajadzhzh <zhiyanjoo1382@gmail.com>
2026-08-27 16:48:21 +0330

    footer done

--- Components/Footer/FollowMe.tsx
+++ Components/Footer/FollowMe.tsx
@@ -5,30 +5,30 @@ export default function FollowMe() {
   return (
     <div className="w-full lg:w-max flex flex-col gap-2">
       <h2 className="text-[14px] text-center">من را دنبال کنید</h2>
-      <div className="flex gap-2">
+      <div className="flex gap-2 w-full justify-center">
         <Link
         href={""}
         className="w-max p-2 rounded-full bg-(--surface) hover:bg-(--surface-hover) group"
       >
-        <FaInstagram size={23} className="group-hover:text-red-500" />
+        <FaInstagram size={20} className="group-hover:text-red-500" />
       </Link>
       <Link
         href={""}
         className="w-max p-2 rounded-full bg-(--surface) hover:bg-(--surface-hover) group"
       >
-        <FaTelegram size={23} className="group-hover:text-blue-700" />
+        <FaTelegram size={20} className="group-hover:text-blue-700" />
       </Link>
       <Link
         href={""}
         className="w-max p-2 rounded-full bg-(--surface) hover:bg-(--surface-hover) group"
       >
-        <FaGithub size={23} className="group-hover:text-gray-500" />
+        <FaGithub size={20} className="group-hover:text-gray-500" />
       </Link>
       <Link
         href={""}
         className="w-max p-2 rounded-full bg-(--surface) hover:bg-(--surface-hover) group"
       >
-        <FaLinkedin size={23} className="group-hover:text-blue-500" />
+        <FaLinkedin size={20} className="group-hover:text-blue-500" />
       </Link>
       </div>
     </div>
--- Components/Footer/Footer.tsx
+++ Components/Footer/Footer.tsx
@@ -1,11 +1,18 @@
+import { Mail, MapPin } from "lucide-react";
 import FollowMe from "./FollowMe";
 
 export default function Footer() {
   return (
-    <footer className="w-full border-t border-(--border) Container flex items-center justify-between">
-        <div></div>
+    <footer className="w-full pb-4 border-t border-(--border) Container flex flex-col md:flex-row gap-5 items-center justify-between">
+        <p className="text-nowrap flex items-end gap-1 text-[14px]">
+            <MapPin />
+            ایران ، کرج
+        </p>
         <FollowMe />
-        <div></div>
+        <p className="text-nowrap flex items-end gap-1 text-[14px]">
+            <Mail />
+            zhiyanjoo1382@gmail.com
+        </p>
     </footer>
   );
 }
