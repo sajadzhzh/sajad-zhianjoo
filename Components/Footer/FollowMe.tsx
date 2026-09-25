8d0cf98add7753a4335e5ac26ba485d12ccf0fc5
Sajadzhzh <zhiyanjoo1382@gmail.com>
2026-09-25 18:49:30 +0330

    Footer done

--- Components/Footer/FollowMe.tsx
+++ Components/Footer/FollowMe.tsx
@@ -1,35 +1,62 @@
+"use client";
+
+import { GetSocialMedia } from "@/Actions/SiteSettings";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { FaGithub, FaInstagram, FaLinkedin, FaTelegram } from "react-icons/fa";
 
 export default function FollowMe() {
+  const [instagram, setInstagram] = useState("");
+  const [telegram, setTelegram] = useState("");
+  const [linkedin, setLinkedin] = useState("");
+  const [github, setGithub] = useState("");
+
+  useEffect(() => {
+    const request = async () => {
+      const res = await GetSocialMedia();
+
+      if (res.success) {
+        setInstagram(res.data.instagram);
+        setTelegram(res.data.telegram);
+        setLinkedin(res.data.linkedin);
+        setGithub(res.data.github);
+      } else {
+        setInstagram("");
+        setTelegram("");
+        setLinkedin("");
+        setGithub("");
+      }
+    };
+    request();
+  }, []);
   return (
     <div className="w-full lg:w-max flex flex-col gap-2">
       <h2 className="text-[14px] text-center">من را دنبال کنید</h2>
       <div className="flex gap-2 w-full justify-center">
         <Link
-        href={""}
-        className="w-max p-2 rounded-full bg-(--surface) hover:bg-(--surface-hover) group"
-      >
-        <FaInstagram size={20} className="group-hover:text-red-500" />
-      </Link>
-      <Link
-        href={""}
-        className="w-max p-2 rounded-full bg-(--surface) hover:bg-(--surface-hover) group"
-      >
-        <FaTelegram size={20} className="group-hover:text-blue-700" />
-      </Link>
-      <Link
-        href={""}
-        className="w-max p-2 rounded-full bg-(--surface) hover:bg-(--surface-hover) group"
-      >
-        <FaGithub size={20} className="group-hover:text-gray-500" />
-      </Link>
-      <Link
-        href={""}
-        className="w-max p-2 rounded-full bg-(--surface) hover:bg-(--surface-hover) group"
-      >
-        <FaLinkedin size={20} className="group-hover:text-blue-500" />
-      </Link>
+          href={instagram}
+          className="w-max p-2 rounded-full bg-(--surface) hover:bg-(--surface-hover) group"
+        >
+          <FaInstagram size={20} className="group-hover:text-red-500" />
+        </Link>
+        <Link
+          href={telegram}
+          className="w-max p-2 rounded-full bg-(--surface) hover:bg-(--surface-hover) group"
+        >
+          <FaTelegram size={20} className="group-hover:text-blue-700" />
+        </Link>
+        <Link
+          href={github}
+          className="w-max p-2 rounded-full bg-(--surface) hover:bg-(--surface-hover) group"
+        >
+          <FaGithub size={20} className="group-hover:text-gray-500" />
+        </Link>
+        <Link
+          href={linkedin}
+          className="w-max p-2 rounded-full bg-(--surface) hover:bg-(--surface-hover) group"
+        >
+          <FaLinkedin size={20} className="group-hover:text-blue-500" />
+        </Link>
       </div>
     </div>
   );
--- Components/Footer/Footer.tsx
+++ Components/Footer/Footer.tsx
@@ -1,18 +1,42 @@
+"use client";
+
 import { Mail, MapPin } from "lucide-react";
 import FollowMe from "./FollowMe";
+import { useEffect, useState } from "react";
+import { GetAddress } from "@/Actions/SiteSettings";
 
 export default function Footer() {
+  const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    const request = async () => {
+      const res = await GetAddress();
+
+      if (res.success) {
+        setAddress(res.data.address);
+        setEmail(res.data.email);
+      } else {
+        setAddress("");
+        setEmail("");
+      }
+    };
+    request();
+  }, []);
   return (
-    <footer className="w-full pb-4 border-t border-(--border) Container flex flex-col md:flex-row gap-5 items-center justify-between" id="contact">
-        <p className="text-nowrap flex items-end gap-1 text-[14px]">
-            <MapPin />
-            ایران ، کرج
-        </p>
-        <FollowMe />
-        <p className="text-nowrap flex items-end gap-1 text-[14px]">
-            <Mail />
-            zhiyanjoo1382@gmail.com
-        </p>
+    <footer
+      className="w-full pb-4 border-t border-(--border) Container flex flex-col md:flex-row gap-5 items-center justify-between"
+      id="contact"
+    >
+      <p className="text-nowrap flex items-end gap-1 text-[14px]">
+        <MapPin />
+        {address}
+      </p>
+      <FollowMe />
+      <p className="text-nowrap flex items-end gap-1 text-[14px]">
+        <Mail />
+        {email}
+      </p>
     </footer>
   );
 }
