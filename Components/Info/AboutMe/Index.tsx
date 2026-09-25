752598dac8ed6625f6513d5d1a71cbe7d723712f
Sajadzhzh <zhiyanjoo1382@gmail.com>
2026-09-25 18:28:02 +0330

    Resume done

--- Components/Info/AboutMe/Index.tsx
+++ Components/Info/AboutMe/Index.tsx
@@ -1,23 +1,69 @@
-"use client"
+"use client";
 
+import { GetResume } from "@/Actions/Resume";
+import { GetAboutMe } from "@/Actions/SiteSettings";
+import Loading from "@/app/Loading";
 import Button from "@/Components/Button/Button";
 import { scrollAnimation } from "@/Helper/Animation";
 import { Download } from "lucide-react";
+import { useEffect, useState } from "react";
+
+type ResumeType = {
+  link: string;
+  is_active: boolean;
+};
 
 export default function AboutMe() {
-  return (
-    <div ref={scrollAnimation("fade-right")} className="md:col-span-2 xl:col-span-1 border border-(--border) rounded-xl px-8 py-7 flex flex-col gap-4">
-      <h2 className="text-xl font-bold">درباره من</h2>
-      <p className="text-[12px] text-justify text-(--muted)">
-        من سجاد هستم، توسعه‌دهنده Full Stack با تمرکز بر Frontend. به ساخت
-        رابط‌های کاربری مدرن، سریع و واکنش‌گرا علاقه‌مندم و در کنار آن تجربه
-        توسعه Backend، API و دیتابیس را نیز دارم.
-      </p>
+  const [aboutMe, setAboutMe] = useState("");
+  const [resume, setResume] = useState<ResumeType[]>([]);
+  const [loading, setLoading] = useState(true);
 
-      <Button theme="primary">
-        <Download />
-        دانلود رزومه
-      </Button>
+  useEffect(() => {
+    const request = async () => {
+      const resAboutMe = await GetAboutMe();
+      const resResume = await GetResume();
+
+      if (resAboutMe.success) {
+        setAboutMe(resAboutMe.data.about_me);
+        setLoading(false);
+      } else {
+        setAboutMe("");
+        setLoading(false);
+      }
+      if (resResume.success) {
+        setResume(resResume.data);
+        setLoading(false);
+      } else {
+        setResume([]);
+        setLoading(false);
+      }
+    };
+    request();
+  }, []);
+  return (
+    <div
+      ref={scrollAnimation("fade-right")}
+      className="opacity-0 md:col-span-2 xl:col-span-1 border border-(--border) rounded-xl px-8 py-7 flex flex-col gap-4"
+    >
+      {aboutMe && (
+        <>
+          <h2 className="text-xl font-bold">درباره من</h2>
+          <p className="text-[12px] text-justify text-(--muted)">{aboutMe}</p>
+        </>
+      )}
+
+      {loading && <Loading />}
+
+      {resume.length > 0 && (
+        <Button
+          theme="primary"
+          onClick={() => window.open(resume[0].link, "_blank")}
+          disAbled={resume[0].is_active ? false : true}
+        >
+          <Download />
+          دانلود رزومه
+        </Button>
+      )}
     </div>
   );
 }
